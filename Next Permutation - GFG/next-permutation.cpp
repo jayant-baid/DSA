37bf8e64d87440c587cdb269649400ad22c17224
Jayant Baid <79745190+jayant-baid@users.noreply.github.com>
2023-06-01 23:24:54 +0530

    Added solution - LeetHub

--- Next Permutation - GFG/next-permutation.cpp
+++ Next Permutation - GFG/next-permutation.cpp
@@ -11,7 +11,27 @@ class Solution{
 public:
     vector<int> nextPermutation(int n, vector<int> arr){
         // code here
-        next_permutation(arr.begin(), arr.end());
+        int index=-1;
+        for(int i=n-2;i>=0;i--){
+            if(arr[i] < arr[i+1]){
+                index=i;
+                break;
+            }
+        }
+        
+        if(index==-1){
+            reverse(arr.begin(), arr.end());
+            return arr;
+        }
+        
+        for(int i=n-1;i>index;i--){
+            if(arr[i]>arr[index]){
+                swap(arr[i], arr[index]);
+                break;
+            }
+        }
+        reverse(arr.begin() + index+1, arr.end());
+        
         return arr;
     }
 };
