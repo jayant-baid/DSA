1fb762871a1b90126880dec23c9230919b6cc5df
Jayant Baid <79745190+jayant-baid@users.noreply.github.com>
2023-08-10 22:40:02 +0530

    Time: 4 ms (92.44%), Space: 14 MB (64.60%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void solve(vector<vector<int>>& image, int oldcolor, int newcolor, int i, int j){
+        if(i<0 || j<0 || i==image.size() || j==image[0].size() || image[i][j] != oldcolor)
+            return;
+        
+        image[i][j]=newcolor;
+        
+        int delrow[]={1, 0, -1, 0};
+        int delcol[]={0, 1, 0, -1};
+        
+        for(int delta=0;delta<4;delta++){
+            solve(image, oldcolor, newcolor, i+delrow[delta], j+delcol[delta]);
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int oldColor=image[sr][sc];
+        if(oldColor == color)
+            return image;
+        
+        solve(image, oldColor, color, sr, sc);
+        return image;
+    }
+};
