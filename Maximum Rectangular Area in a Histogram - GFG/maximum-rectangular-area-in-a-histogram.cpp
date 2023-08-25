5c82755c53e3e72b7b840ec3776b7cbc664075a4
Jayant Baid <79745190+jayant-baid@users.noreply.github.com>
2023-08-25 10:56:37 +0530

    Added solution - LeetHub

--- Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
+++ Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
@@ -10,45 +10,23 @@ class Solution
     //Function to find largest rectangular area possible in a given histogram.
     long long getMaxArea(long long arr[], int n)
     {
-        vector<long long> prev(n);
-        vector<long long> next(n);
-        
-        stack<long long> st;
-        
-        for(int i=0;i<n;i++){
-            int curr=arr[i];
-            while(!st.empty() && arr[st.top()] >= curr)
-                st.pop();
-            if(st.empty())
-                prev[i]=0;
-            else
-                prev[i]=st.top()+1;
-                
-            st.push(i);
-        }
-        while(!st.empty())  st.pop();
-        
-        for(int i=n-1;i>=0;i--){
-            int curr=arr[i];
-            
-            while(!st.empty() && arr[st.top()] >= curr)
-                st.pop();
-                
-            if(st.empty())
-                next[i]=n-1;
-            else
-                next[i]=st.top()-1;
-                
-            st.push(i);
+        stack<long long> s;
+        long long area=0;
+        for(int i=0;i<=n;i++){
+            while(!s.empty() && (i==n || arr[s.top()] >arr[i])){
+                long long length=arr[s.top()];
+                s.pop();
+                long long width;
+                if(s.empty())
+                    width=i;
+                else
+                    width=i-s.top()-1;
+                    
+                area=max(area, length*width);
+            }
+            s.push(i);
         }
         
-        long long area=INT_MIN;
-        for(int i=0;i<n;i++){
-            long long l=arr[i];
-            
-            long long b=next[i] - prev[i] + 1;
-            area=max(area, l*b);
-        }
         return area;
     }
 };
