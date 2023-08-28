c6f0309ff934d0280368b699369f395e89f9713e
Jayant Baid <79745190+jayant-baid@users.noreply.github.com>
2023-08-28 11:35:34 +0530

    Codestudio Sum of minimum and maximum elements of all subarrays of size “K”.cpp

--- Sum of minimum and maximum elements of all subarrays of size “K”.cpp
+++ Sum of minimum and maximum elements of all subarrays of size “K”.cpp
@@ -1,27 +1,65 @@
+// Middle Approach
+// #include <bits/stdc++.h> 
+// long long sumOfMaxAndMin(vector<int> &nums, int n, int k) {
+// 	long long ans=0;
+//     multiset<int> st;
+    
+//     for(int i=0;i<k;i++)
+//         st.insert(nums[i]);
+
+//     int mini=*st.begin();
+//     int maxi=*st.rbegin();
+
+//     ans+=mini+maxi;
+    
+//     int startindex=0;
+    
+//     for(int i=k;i<n;i++){
+//         st.insert(nums[i]);
+//         st.erase(st.find(nums[startindex]));
+        
+//         int mini=*st.begin();
+//         int maxi=*st.rbegin();
+
+//         ans+=mini+maxi;
+//         startindex++;
+//     }
+//     return ans;
+// }
+
+// Optimized Approad
 #include <bits/stdc++.h> 
 long long sumOfMaxAndMin(vector<int> &nums, int n, int k) {
 	long long ans=0;
-    multiset<int> st;
-    
-    for(int i=0;i<k;i++)
-        st.insert(nums[i]);
+    deque<int> maxi;
+    deque<int> mini;
 
-    int mini=*st.begin();
-    int maxi=*st.rbegin();
+    for(int i=0;i<k;i++){
+        while(!maxi.empty() && nums[maxi.front()] <= nums[i])
+            maxi.pop_front();
+        while(!mini.empty() && nums[mini.front()] >= nums[i])
+            mini.pop_front();
 
-    ans+=mini+maxi;
-    
-    int startindex=0;
-    
+        maxi.push_front(i);
+        mini.push_front(i);
+    }
+    ans+=nums[maxi.back()] + nums[mini.back()];
+
     for(int i=k;i<n;i++){
-        st.insert(nums[i]);
-        st.erase(st.find(nums[startindex]));
-        
-        int mini=*st.begin();
-        int maxi=*st.rbegin();
+        while(!maxi.empty() && i-maxi.back()>=k)
+            maxi.pop_back();
+        while(!mini.empty() && i-mini.back()>=k)
+            mini.pop_back();
 
-        ans+=mini+maxi;
-        startindex++;
+        while(!maxi.empty() && nums[maxi.front()] <= nums[i])
+            maxi.pop_front();
+        while(!mini.empty() && nums[mini.front()] >= nums[i])
+            mini.pop_front();
+
+        maxi.push_front(i);
+        mini.push_front(i);
+        ans+=nums[maxi.back()] + nums[mini.back()];
     }
     return ans;
 }
+// We can interchange front() and back() and get correct ans
