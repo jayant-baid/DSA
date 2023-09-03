acdfc5151d9d2027e368db62ab06a1957e806926
Jayant Baid <79745190+jayant-baid@users.noreply.github.com>
2023-09-03 12:41:40 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (8.60%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        vector<int> ans;
-        int cnt=1;
+        map<int, int> mpp;
         sort(arr.begin(), arr.end());
-        for(int i=1;i<arr.size();i++){
-            if(arr[i] == arr[i-1])
-                cnt++;
-            else{
-                ans.push_back(cnt);
-                cnt=1;
-            }
+        for(int i=0;i<arr.size();i++)
+            mpp[arr[i]]++;
+        
+        set<int> st;
+        for(auto it: mpp){
+            st.insert(it.second);
         }
-        ans.push_back(cnt);
-        sort(ans.begin(), ans.end());
-        for(int i=0;i<ans.size()-1;i++){
-            if(ans[i] == ans[i+1])
-                return false;
-        }
-        return true;
+            
+        
+        return mpp.size() == st.size();
         
     }
 };
