e963b9672e6527cd8e003b6830963e508bf1e5c4
Jayant Baid <79745190+jayant-baid@users.noreply.github.com>
2023-09-21 23:14:14 +0530

    Time: 183 ms (94.92%), Space: 134.9 MB (44.62%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,23 +5,23 @@ public:
         deque<int> dq;
 
         for(int i=0;i<k;i++){
-            while(!dq.empty() && arr[dq.front()] <= arr[i])
-                dq.pop_front();
+            while(!dq.empty() && arr[dq.back()] <= arr[i])
+                dq.pop_back();
             
-            dq.push_front(i);
+            dq.push_back(i);
         }
-        ans.push_back(arr[dq.back()]);
+        ans.push_back(arr[dq.front()]);
 
         for(int i=k;i<arr.size();i++){
-            while(!dq.empty() && i-dq.back()>=k)
-                dq.pop_back();
-           
-            while(!dq.empty() && arr[dq.front()] <= arr[i])
+            while(!dq.empty() && i-dq.front()>=k)
                 dq.pop_front();
            
-            dq.push_front(i);
+            while(!dq.empty() && arr[dq.back()] <= arr[i])
+                dq.pop_back();
+           
+            dq.push_back(i);
           
-            ans.push_back(arr[dq.back()]);
+            ans.push_back(arr[dq.front()]);
         }
         return ans;
     }
