9a84c4e2fb1c2684f11ed947f2e3886c1cd7bd15
Jayant Baid <79745190+jayant-baid@users.noreply.github.com>
2023-09-30 21:43:24 +0530

    Approach 3 added

--- Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.cpp
+++ Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.cpp
@@ -1,104 +1,13 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
+//User function Template for C++
+/*
+Structure of the node of the binary tree is as
 struct Node
 {
-    int data;
-    struct Node *left;
-    struct Node *right;
+	int data;
+	struct Node *left, *right;
 };
-Node* newNode(int val)
-{
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-    
-    return temp;
-}
-Node* buildTree(string str)
-{   
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
-    // string after spliting by space
-    vector<string> ip;
-    
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-        
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-        
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-        
-    // Starting from the second element
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-            
-        // Get the current node's value from the string
-        string currVal = ip[i];
-            
-        // If the left child is not null
-        if(currVal != "N") {
-                
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-            
-        // For the right child
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-            
-        // If the right child is not null
-        if(currVal != "N") {
-                
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-    
-    return root;
-}
-int kthAncestor(Node *root, int k, int node);
-
-int main()
-{
-    int t;
-	scanf("%d ",&t);
-    while(t--)
-    {
-        int k , node;
-        scanf("%d ",&k);
-        scanf("%d ",&node);
-        string s;
-		getline(cin,s);
-		Node* root = buildTree(s);
-		cout<<kthAncestor(root,k,node)<<endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
+*/
+// your task is to complete this function
 
 
 //User function Template for C++
@@ -111,32 +20,39 @@ struct Node
 };
 */
 // your task is to complete this function
-bool solve(Node* root, int node, vector<int>& parent){
+bool solve(Node* root, int node, int& k, int& ans){
     if(root == NULL)
         return false;
        
-    parent.push_back(root->data); 
-    if(root->data == node)
+    if(root->data == node){
+        k--;
         return true;
+    }
         
-    bool left = solve(root->left, node, parent);
-    if(left)
+    bool left = solve(root->left, node, k, ans);
+    if(left){
+        if(k==0){
+            ans=root->data;
+            return false;
+        }
+        k--;
         return true;
-    bool right = solve(root->right, node, parent);
-    if(right)
+    }
+    bool right = solve(root->right, node, k, ans);
+    if(right){
+        if(k==0){
+            ans=root->data;
+            return false;
+        }
+        k--;
         return true;
-    
-    parent.pop_back();
-    
-    return false;
+    }
+    // return false;
 }
 int kthAncestor(Node *root, int k, int node)
 {
-    vector<int> parent;
-    solve(root, node, parent);
-    
-    int n=parent.size();
-    if(n-k-1<0)
-        return -1;
-    return parent[n-k-1];
+    int ans=-1;
+    solve(root, node, k, ans);
+ 
+    return ans;
 }
